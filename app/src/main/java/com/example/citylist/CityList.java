33417b8d5997285d689d0d14b44f4cad2a4e3955
safwan-zaher <asif1907066@stud.kuet.ac.bd>
2023-03-29 00:23:07 +0600

    Changed

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -23,13 +24,51 @@ public class CityList {
     }
 
     /**
+     * This is for deleting city
+     * @param city
+     */
+    public void delete(City city)
+    {
+        if (!cities.contains(city))
+        {
+            throw new IllegalArgumentException();
+        }
+        cities.remove(city);
+    }
+
+
+    /**
+     * This is for counting city
+     * @return
+     */
+    public int count()
+    {
+        return cities.size();
+    }
+
+
+
+
+    /**
      * This returns a sorted list of cities
      * @return
      *      Return the sorted list of cities
      */
-    public List<City> getCities() {
+    public List<City> getCities(int param) {
         List<City> cityList = cities;
-        Collections.sort(cityList);
+        if(param==1)
+        {
+            Collections.sort(cityList);
+        }
+        else
+        {
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City t1) {
+                    return city.getProvinceName().compareTo(t1.getProvinceName());
+                }
+            });
+        }
         return cityList;
     }
 }
